68ea4dcaa00da2e81302b1a695ebc2963ce71695
tomchentw <developer@tomchentw.com>
2014-05-08 11:14:45 +0800

    feat(scaffold): template with public from hw2
    * add .gitignore from gitignore.io
    * add public folder from hw2
    * change paths in index to refer from public
    * add items controller and require in index.js

---
+++ controllers/items.js
@@ -0,0 +1,25 @@
+exports.list = function (req, res) {
+  // res.json([{
+  //   text: 'Example TODOLIST',
+  //   isDone: false
+  // }]);
+};
+
+exports.create = function (req, res) {
+  // var newTodo = req.body;
+  // newTodo.id = 1;// 2, 3, .....
+  
+  // res.json(newTodo);
+};
+
+exports.update = function (req, res) {
+
+};
+
+exports.reposition = function (req, res) {
+
+};
+
+exports.delete = function (req, res) {
+
+};
