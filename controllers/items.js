0ceb7499826d398e83948d9fe5273dd892e00def
Ming-Yuan Jian <b99902067@csie.ntu.edu.tw>
2014-05-19 23:16:46 +0800

    complete javascripts

--- controllers/items.js
+++ controllers/items.js
@@ -1,25 +1,75 @@
-exports.list = function (req, res) {
-  // res.json([{
-  //   text: 'Example TODOLIST',
-  //   isDone: false
-  // }]);
+var fs = require('fs');
+var itemsFile = "lists.json";
+
+if (!fs.existsSync(itemsFile)){
+    fs.writeFileSync(itemsFile, JSON.stringify({next_id: 1, items: []}));
+}
+var data = JSON.parse(fs.readFileSync(itemsFile));
+//var next_id = data.next_id;
+//var items = data.items;
+
+// GET /items
+exports.list = function(req, res){
+    console.log(data);
+    // res.json([{
+    //   text: 'Example TODOLIST',
+    //   isDone: false
+    // }]);
+    res.json(data.items);
 };
 
-exports.create = function (req, res) {
-  // var newTodo = req.body;
-  // newTodo.id = 1;// 2, 3, .....
-  
-  // res.json(newTodo);
+// POST /items
+exports.create = function(req, res){
+    var newTodo = req.body;
+    newTodo.done = (newTodo.done === "true")? true: false;
+    newTodo.id = data.next_id;
+    data.next_id++;
+    data.items.push(newTodo);
+    fs.writeFileSync(itemsFile, JSON.stringify(data));
+    res.json(newTodo);
+    console.log(data);
 };
 
-exports.update = function (req, res) {
-
+// PUT /items/:id
+exports.update = function(req, res){
+    var newTodoId = parseInt(req.param('id'));
+    var newTodo = req.body;
+    newTodo.done = (newTodo.done === "true")? true: false;
+    newTodo.id = newTodoId;
+    data.items.forEach(function(element, idx, array){
+        if (element.id === this.id && element.done !== this.done){
+            element.done = this.done;
+            array.splice(idx, 1);
+            array.splice(0, 0, element);
+        }
+    }, newTodo);
+    fs.writeFileSync(itemsFile, JSON.stringify(data));
+    res.json(newTodo);
+    console.log(data);
 };
 
-exports.reposition = function (req, res) {
-
+// PUT /items/:id/reposition/:new_position
+exports.reposition = function(req, res){
+    var repTodoId = parseInt(req.param('id'));
+    var newPosition = data.items.length - parseInt(req.param('new_position')) - 1;
+    data.items.forEach(function(element, idx, array){
+        if (element.id === this.id && idx !== this.newIdx){
+            array.splice(idx, 1);
+            array.splice(this.newIdx, 0, element);
+        }
+    }, {id: repTodoId, newIdx: newPosition});
+    fs.writeFileSync(itemsFile, JSON.stringify(data));
+    res.json({id: repTodoId, newIdx: newPosition});
+    console.log(data);
 };
 
-exports.delete = function (req, res) {
-
+// DELETE /items/:id
+exports.delete = function(req, res){
+    var delTodoId = parseInt(req.param('id'));
+    data.items = data.items.filter(function(element){
+        return (element.id !== delTodoId);
+    });
+    fs.writeFileSync(itemsFile, JSON.stringify(data));
+    res.json(data.items);
+    console.log(data);
 };
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,143 @@
 (function(){
+    "use strict";
 
-// 插入 <ul> 之 <li> 樣板
-var tmpl = '<li><input type="text"><span></span></li>',
-    addButton = $('#add'),
-    connected = $('.connected'),      // 三個 <ul>
-    placeholder = $('#placeholder'),  // 三個 <ul> 的容器
-    mainUl = $('.main'),              // main <ul>
-    deleteUl = $('.delete'),          // delete <ul>
-    doneUl = $('.done');              // done <ul>
+    // 插入 <ul> 之 <li> 樣板
+    var tmpl = '<li><input type="text" placeholder="New Task..."><span></span></li>',
+        connected = $('ul.connected'),      // 三個 <ul>
+        placeholder = $('#placeholder'),  // 三個 <ul> 的容器
+        addButton = $('#add'),
+        mainUL = $('ul.main'),              // main <ul>
+        deleteUL = $('ul.delete'),          // delete <ul>
+        doneUL = $('ul.done');              // done <ul>
 
+    // make an <li> element with specified text
+    var makeItem = function (text){
+        var li = $(tmpl);
+        if (text){
+            li.find('input').val(text);
+            li.find('span').text(text);
+        }
+        return li;
+    };
+
+    $.get("/items", {}, function(data, status){
+        $.each(data, function(){
+            var li = makeItem(this.text);
+            if (this.done){
+                li.addClass("is-done");
+            }
+            li.attr('title', this.id);
+            li.prependTo(mainUL);
+        });
+    });
+    
+
+    mainUL.sortable({
+        start: function(event, ui){
+            placeholder.addClass('is-dragging');
+        },
+        stop: function(event, ui){
+            placeholder.removeClass('is-dragging');
+            var reqURL = '/items/' + ui.item.attr('title') + '/reposition/' + ui.item.index();
+            console.log(reqURL);
+            $.ajax({
+                type: 'PUT',
+                url: reqURL,
+                success: function(data, status){
+                    console.log(data);
+                    console.log(status);
+                }
+            });
+        },
+        connectWith: "ul.connected",
+        tolerance: "pointer"
+    });
+
+    deleteUL.sortable({
+        receive: function(event, ui){
+            console.log(ui.item.attr('title'));
+            $.ajax({
+                type: 'DELETE',
+                url: '/items/' + ui.item.attr('title'),
+                success: function(data, status){
+                    console.log(status);
+                }
+            });
+            ui.item.remove();
+        },
+        connectWith: "ul.connected",
+        tolerance: "pointer"
+    });
+
+    doneUL.sortable({
+        receive: function(event, ui){
+            ui.item.addClass("is-done").appendTo(mainUL);
+            $.ajax({
+                type: 'PUT',
+                url: '/items/' + ui.item.attr('title'),
+                data: {done: true},
+                success: function(data, status){
+                    console.log(status);
+                }
+            });
+        },
+        connectWith: "ul.connected",
+        tolerance: "pointer"
+    });
+
+    // event handlers
+    addButton.click(function(){
+        var li = $(".is-editing");
+        if (li.length === 0){
+            var li = makeItem().addClass('is-editing').prependTo(mainUL);
+        }
+        li.find('input').focus();
+    });
+
+    mainUL.on('keypress', 'input', function(e){
+        var li;
+
+        // when Enter is pressed, e.which === 13
+        if (e.which === 13){
+            // "this" is the <input>
+            li = $(this).parents('li');
+            if (li.find('input').val() === ""){
+                return;
+            }
+            var txt = li.find('input').val();
+            li.find('span').text(txt);
+            li.removeClass('is-editing');
+
+            $.ajax({
+                type: 'POST',
+                url: '/items',
+                data: {done: false, text: txt},
+                context: li,
+                success: function(data, status){
+                    console.log(data);
+                    $(this).attr('title', data.id);
+                },
+                dataType: 'json'
+            });
+        }
+    });
+    
+    function save(){
+        var data = [];
+        mainUL.find('li').each(function(){
+            if ($(this).hasClass("is-editing")){
+                return;
+            }
+
+            if ($(this).hasClass("is-done")){
+                data.push({done: true,
+                    text: $(this).find('span').text()});
+            }
+            else {
+                data.push({done: false,
+                    text: $(this).find('span').text()});
+            }
+        });
+    }
 }());
+
